3d71f7eb3cd046954ec36894a6f7cad375bc4741
Scott Jiang <scottjiang19@gmail.com>
2024-03-03 01:14:59 -0800

    change about me

--- src/Components/AboutMe.js
+++ src/Components/AboutMe.js
@@ -11,24 +11,10 @@ const AboutMe = () => {
 			<Header Section="About Me" />
 			<div className="descriptionAndPhoto">
 				<div className="description">
-					<p className="noPadding">
-						Hi! My name’s Scott and I’m an undergraduate student studying
-						electrical engineering at UBC. I started learning web development
-						right after my first year of university before doing my first
-						Hackathon, where I learned a ton about HTML/CSS and Javascript.
-					</p>
 					<p>
-						So far, I’ve completed an internship at a{" "}
-						<a href="https://tryknowhow.com/" target="_blank">
-							startup
-						</a>{" "}
-						based in Calgary, and now i’m working at{" "}
-						<a href="https://legacy.smarttech.com/en/lumio" target="_blank">
-							SMART Technologies
-						</a>{" "}
-						as a Software Engineer Intern.
+						Hi, thanks for stopping by! My name's Scott and I'm an undergraduate student studying electrical engineering at UBC. Feel free to take a look around!	
 					</p>
-					<p>These are some technologies I’ve worked with:</p>
+					<p>These are some technologies I’ve worked with:</p> 
 					<UsersLogos users={Skills} />
 				</div>
 				<div className="Image">
